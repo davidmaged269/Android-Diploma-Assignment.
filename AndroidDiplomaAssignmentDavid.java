9101cd7a59f542ba8d8db9b549a44e1c2df2a118
davidmaged269 <140670957+davidmaged269@users.noreply.github.com>
2023-09-03 21:39:29 +0300

    Add files via upload
    Problem 1

---
+++ AndroidDiplomaAssignmentDavid.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package android.diploma.assignment;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David
+ */
+public class AndroidDiplomaAssignmentDavid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input= new Scanner(System.in);
+        
+        System.out.print("Please enter a number : ");
+        
+         int num=input.nextInt();
+         
+         System.out.println(num);
+    }
+    
+}
