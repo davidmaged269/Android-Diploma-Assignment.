7aa7f5d919bec3cc8911608a4ac856c736a65987
davidmaged269 <140670957+davidmaged269@users.noreply.github.com>
2023-09-03 23:19:07 +0300

    Add files via upload

---
+++ Problem11.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problem.pkg11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David
+ */
+public class Problem11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Insert an integer : ");
+        
+        int number=input.nextInt();
+        
+        String result=(number%2==0) ? "Even": "Odd";
+        
+        System.out.println(result);
+        
+       
+    }
+    
+}
