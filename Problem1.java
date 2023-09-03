914ce4ec0fb90bf56c52513ae4d881ac47435a65
davidmaged269 <140670957+davidmaged269@users.noreply.github.com>
2023-09-03 22:08:18 +0300

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problem.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David
+ */
+public class Problem1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input=new Scanner(System.in);
+        
+        System.out.print("Enter a number : ");
+        
+        int number=input.nextInt();
+        
+        System.out.println(number);
+    }
+    
+}
